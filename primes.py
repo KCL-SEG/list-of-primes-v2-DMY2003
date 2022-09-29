490fd103019260b7b813c82d9f48e387c37c105d
Damian Mitrofan <damian.mitrofan@kcl.ac.uk>
2022-09-29 11:21:51 +0100

    Initial

--- primes.py
+++ primes.py
@@ -2,5 +2,12 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    if number_of_primes <=0:
+        raise ValueError("No <= 0 values.")
     list = []
+    first = 2
+    while len(list) < number_of_primes:
+        pcheck = [first for i in list if first%i == 0]
+        list += [] if pcheck else [first]
+        first += 1
     return list
